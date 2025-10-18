24df69d2e348b40cf4e728c57039287becc20b31
lizardo-andres12 <lizardo.andres12@gmail.com>
2025-10-17 21:14:27 -0400

    add: completed search function

--- src/api/controller/image_search.py
+++ src/api/controller/image_search.py
@@ -34,36 +34,33 @@ class SearchController:
         Returns:
             SearchResponse: The top (limit) many keywords and images.
         """
-        try:
-            image = await self._process_file_upload(file)
-            embedding = self.clip_service.extract_image_features(image)
-            similar_embeddings = self.vector_repository.query_similar(embedding, limit)
+        image = await self._process_file_upload(file)
+        embedding = self.clip_service.extract_image_features(image)
+        similar_embeddings = self.vector_repository.query_similar(embedding, limit)
 
-            similar_ids = [result.id for result in similar_embeddings]
-            similar_metadatas = await self.image_repository.batch_get_image_metadata(
-                similar_ids
+        similar_ids = [result.id for result in similar_embeddings]
+        similar_metadatas = await self.image_repository.batch_get_image_metadata(
+            similar_ids
+        )
+
+        if not similar_embeddings or not similar_metadatas:
+            raise ValueError("One or more queries returned none")
+
+        results = []
+        for image, metadata in zip(similar_embeddings, similar_metadatas):
+            results.append(
+                SimilarImage(
+                    id=image.id,
+                    similarity=image.similarity,
+                    source_url=metadata.source_url,
+                    source_domain=metadata.source_domain,
+                    filename=metadata.filename,
+                    file_size=metadata.file_size,
+                    dimensions=metadata.dimensions,
+                )
             )
 
-            results = []
-            for image, metadata in zip(similar_embeddings, similar_metadatas):
-                results.append(
-                    SimilarImage(
-                        id=image.id,
-                        similarity=image.similarity,
-                        source_url=metadata.source_url,
-                        source_domain=metadata.source_domain,
-                        filename=metadata.filename,
-                        file_size=metadata.file_size,
-                        dimensions=metadata.dimensions,
-                        tags=[],
-                    )
-                )
-
-            return results
-
-        except Exception as e:
-            logger.error(f"Search failed: {e}")
-            raise e
+        return results
 
     async def _process_file_upload(self, file: UploadFile) -> Image.Image:
         """Converts the uploaded file to PIL Image.
@@ -80,13 +77,8 @@ class SearchController:
         finally:
             await file.close()
 
+    """
     def _validate_upload(self, file: UploadFile):
-        """
-        Ensures the uploaded file is an image
-
-        Args:
-            file (UploadFile): The file from HTTP request.
-        """
 
         if not file.content_type or not file.content_type.startswith("image/"):
             raise HTTPException(status_code=400, detail="File must be an image")
@@ -96,40 +88,4 @@ class SearchController:
 
         if not file.filename:
             raise HTTPException(status_code=400, detail="No filename provided")
-
-    def _search_similar_vectors(
-        self, embedding: list[float], limit: int
-    ) -> list[SimilarImage]:
-        """
-        Search for similar vectors in ChromaDB. Uses cosine similarity to find matches.
-
-        Args:
-            embedding (list): Vector embedding list to perform cosine similarity on.
-            limit (int): Upper bound of images to return.
-        Returns:
-            list[dict]: List of image uuids and corresponding vector embeddings.
-        """
-        results = self.vector_repository.query_similar(embedding, limit)
-
-    def _fetch_image_metadata(self, similar_vectors: list[dict]) -> list[SimilarImage]:
-        """
-        Fetch metadata for similar images.
-
-        Args:
-            similar_vectors (list[dict]): The similar vector data with uuid and similarity score.
-        Returns:
-            list[SimilarImage]: List of SimilarImage result objects with uuid, similarity score, and
-            retrieval URLs.
-        """
-        pass
-
-    def _generate_keywords(self, similar_images: list[SimilarImage]) -> list[str]:
-        """
-        Generate keywords from similar images.
-
-        Args:
-            similar_images (list[SimilarImage]): The results of similarity scoring pipeline.
-        Returns:
-            list[str]: List of the top MAX_TAGS frequent tags retrieved by result image uuids.
-        """
-        pass
+    """
